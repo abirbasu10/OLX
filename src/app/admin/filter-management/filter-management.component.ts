41a8840ce268f6bc4939ef3e1557813658991fba
Bipraneel <Bipraneel>
2018-06-11 18:47:16 +0530

    commit

--- src/app/admin/filter-management/filter-management.component.ts
+++ src/app/admin/filter-management/filter-management.component.ts
@@ -4,6 +4,7 @@ import {Category,SubCategory,SubCatFilterMap,Filter,SubCategoryFilterOption} fro
 
 import {CATEGORIES,SUBCATEGORIES,SUBCATFILTERMAP,FILTERS,SUBCATFILTEROPTIONS} from '../../application_mock_Data'
 
+declare const $;
 @Component({
   selector: 'app-filter-management',
   templateUrl: './filter-management.component.html',
@@ -16,18 +17,25 @@ export class FilterManagementComponent implements OnInit {
 
   subCatsByCat : SubCategory[] = null;
 
-  filtersBySubCat : Filter[] = null;
+  currentFilterId : number;
 
+  currentSubCat : any;
+  //filtersBySubCat : Filter[] = null;
+  filtersBySubCat : any[] = [];
   addFilter : boolean = false;
 
   extraInfo : boolean = false;
 
   addDropdown : boolean = false;
 
+  //editDropdown : boolean = false;
+
   currentDropdownVal : string = null;
 
   listOfDropdownVal : any [] = [];
   currentSubCatId:number=0
+
+  editFilterVal : any[] = [];
 
   filterName : string = ""
   filterDesc : string = ""
@@ -50,6 +58,26 @@ export class FilterManagementComponent implements OnInit {
     this.addFilter = false;
   }
 
+  editDropdownVal(filterId){
+    //this.editDropdown = true;
+    this.currentFilterId = filterId
+    var i = 0
+    var value = SUBCATFILTEROPTIONS.find(sCat => sCat.subCategoryDetails.id == this.currentSubCatId).subCatFilterValues.find(sFil => sFil.filterDetails.id == this.currentFilterId).value
+    
+    this.editFilterVal = value
+  
+    $("#editDropdownModal").modal('show')
+  }
+
+  addEditedDropdownValue(){
+    this.editFilterVal.push("")    
+  }
+
+  deleteFilterVal(filterValIndex)
+  {
+    this.editFilterVal.splice(filterValIndex,1)
+  }
+
   saveFilter()
   {
     
@@ -64,6 +92,7 @@ export class FilterManagementComponent implements OnInit {
     (
       {id:filterOptForCurrentSubCat.length+1,filterDetails: {  id:filterForCurrentSubCat.filterDetails.length+1,name:this.filterName,description:this.filterDesc,type:this.filterType,addType:this.filterAddType,extraInfo:this.extraInfo},value:this.listOfDropdownVal},
     )
+    this.getFiltersBySubCat(this.currentSubCat)
 
     this.filterName = null
     this.filterDesc = null
@@ -76,7 +105,8 @@ export class FilterManagementComponent implements OnInit {
     this.listOfDropdownVal = []
 
     this.closeFilter()
-    
+  
+    console.log("SUBCATFILTEROPTIONS",SUBCATFILTEROPTIONS)
   }
 
   onChangeFilterType(event){
@@ -89,6 +119,22 @@ export class FilterManagementComponent implements OnInit {
       this.addDropdown = false;
     }
 
+  }
+
+  saveEditedDropdownVal(){
+    
+    var value = []
+    
+    for (var i = 0, len = this.editFilterVal.length; i < len; i++) {
+    console.log("Val",$("#editDropdownVal"+(i+1)).val())
+    if($("#editDropdownVal"+(i+1)).val())
+    value.push($("#editDropdownVal"+(i+1)).val())
+    }
+    
+    SUBCATFILTEROPTIONS.find(sCat => sCat.subCategoryDetails.id == this.currentSubCatId).subCatFilterValues.find(sFil => sFil.filterDetails.id == this.currentFilterId).value = value
+    console.log("After Adding",SUBCATFILTEROPTIONS)
+    this.getFiltersBySubCat(this.currentSubCat)
+    $("#editDropdownModal").modal('hide')
   }
 
   addDropdownValue(){
@@ -109,12 +155,20 @@ export class FilterManagementComponent implements OnInit {
 
   getFiltersBySubCat(event)
   {
+    this.filtersBySubCat = []
+    this.currentSubCat = event;
     var subCatId = event.value;
     this.currentSubCatId=subCatId 
-    this.filtersBySubCat = SUBCATFILTERMAP.find(filter => filter.subCategoryDetails.id == subCatId ).filterDetails
-
+    var filterVal = SUBCATFILTEROPTIONS.find(filter => filter.subCategoryDetails.id == subCatId ).subCatFilterValues
+    //this.filtersBySubCat = SUBCATFILTERMAP.find(filter => filter.subCategoryDetails.id == subCatId ).filterDetails
+    //this.filtersBySubCat = SUBCATFILTEROPTIONS.find(filter => filter.subCategoryDetails.id == subCatId ).subCatFilterValues
+    for(let val of filterVal){
+    //console.log(" Filter are",val.filterDetails)
+  //    console.log(" Filter Vals are",val.value)
+      this.filtersBySubCat.push({'details':val.filterDetails,'values':val.value})
+    }
     console.log("Filters are : ", this.filtersBySubCat)
-
+    
   }
 
 
